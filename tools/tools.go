57534da18a82bbbc9330bb326bd54ff4f7c58098
sin392 <sin392@gmail.com>
2024-10-08 23:03:18 +0900

    delete unnecessary files

--- tools/tools.go
+++
@@ -1,9 +0,0 @@
-// +build tools
-
-package tools
-
-import (
-	_ "github.com/99designs/gqlgen"
-	_ "google.golang.org/grpc/cmd/protoc-gen-go-grpc"
-	_ "google.golang.org/protobuf/cmd/protoc-gen-go"
-)
